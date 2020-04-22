04af41f12b9fa5c33ad841b74d960db743257ec3
Matt Beachey <mattbeachey@gmail.com>
2020-04-22 14:22:52 -0500

    got loop working

--- assets/script.js
+++ assets/script.js
@@ -12,42 +12,32 @@ let synth = new Tone.Synth().toDestination()
 //     synth.triggerAttackRelease('C4', 2, 0)   
 // })
 
-const loop = new Tone.Loop(function(time) {
-    //triggered every eighth note.
-    console.log(time);
-    synth.triggerAttackRelease("C2", "2n");
-    synth.triggerAttackRelease("D2", "2n", "2n");
-  }, "2n").start(0);
+
 
 
 
 barOne.addEventListener("click", function(){
-    Tone.start()
-    Tone.Transport.start();
-    // melodyOne()
+    
+    melodyOne()
 
 
 })
 
 function melodyOne() {
-    var loop = new Tone.Loop(function(time){
-        //triggered every eighth note. 
+    Tone.start()
+    Tone.Transport.start();
+    const loop = new Tone.Loop(function(time) {
+        //triggered every eighth note.
         console.log(time);
-        synth.triggerAttackRelease('C4', '4n', '4n')
-        // synth.triggerAttackRelease('G4', '4n', (time + 0.5))
+        synth.triggerAttackRelease('C4', '4n', time)
+        synth.triggerAttackRelease('G4', '4n', (time + 0.5))
         // // //rest
-        // synth.triggerAttackRelease('D4', Tone.Time('4n'), (time + 1.5))
+        synth.triggerAttackRelease('D4', Tone.Time('4n'), (time + 1.5))
         // // rest
-        // synth.triggerAttackRelease('A4', '4n', (time + 2.5))
+        synth.triggerAttackRelease('A4', '4n', (time + 2.5))
         // // rest
         // // rest
-    }, "8n").start(0);
+      }, 4).start(0);
 
 }
 
-// synth.triggerAttackRelease('C4', '4n', '8n')
-// synth.triggerAttackRelease('E4', '8n', Tone.Time('4n') + Tone.Time('8n'))
-// synth.triggerAttackRelease('G4', '16n', '2n')
-// synth.triggerAttackRelease('B4', '16n', Tone.Time('2n') + Tone.Time('8t'))
-// synth.triggerAttackRelease('G4', '16', Tone.Time('2n') + Tone.Time('8t')*2)
-// synth.triggerAttackRelease('E4', '2n', '0:3')
